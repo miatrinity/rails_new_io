126f78a56ddc61525867b582a36b3dbba1e51146
MiaTrinity <mia.trinity.codes@gmail.com>
2020-10-26 14:52:33 +0100

    Replaced with native HTML5 Solution

--- app/javascript/controllers/row_selection_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus"
-
-export default class extends Controller {
-  static targets = [ "checked" ]
-    
-  update() {      
-    this.checkedTarget.checked = true
-  }
-}
