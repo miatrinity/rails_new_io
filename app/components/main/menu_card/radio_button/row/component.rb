ce99d588443484974cdf8054526799331f9992bb
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-11-10 11:06:22 +0100

    Refactor - has_image and menu_card_item

--- app/components/main/menu_card/radio_button/row/component.rb
+++ app/components/main/menu_card/radio_button/row/component.rb
@@ -3,9 +3,11 @@ module Main
     module RadioButton
       module Row
         class Component < ViewComponent::Base
-          def initialize(title:, description:, command_output:, checked:)
-            @title       = title
+          def initialize(title:, has_image:false, description:, menu_card_id:, command_output:, checked:)
+            @title = title
+            @has_image = has_image
             @description = description
+            @menu_card_id = menu_card_id
             @command_output = command_output
             @checked = checked
           end
