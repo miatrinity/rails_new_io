a7afb3c1e6efb5da33a92afecd01d808bcafaf3c
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-11-16 13:34:10 +0100

    MOAR (Mother of all refactorings)  💩 ➡️ ✅ 😅

---
+++ app/components/tabs/classics/component.rb
@@ -0,0 +1,11 @@
+module Tabs
+  module Classics
+    class Component < ViewComponent::Base
+      def initialize(initial_state:, state_translation:, all_menu_cards_in_all_states:)
+        @all_menu_cards_in_all_states = all_menu_cards_in_all_states
+        @initial_state                = initial_state
+        @state_translation            = state_translation
+      end
+    end
+  end
+end
