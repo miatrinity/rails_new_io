ecefbfebd7da75844d112065170719ecea6ced0d
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-11-17 09:46:14 +0100

    Add a primitive first version of pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# nodoc
+class PagesController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'initial_setup#index'
+  
+  resources :pages, only: :show
 end
