1459ec0de5289e7c724f0334811d9f14696faaab
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-11-23 10:58:30 +0100

    Remove whitespace/comments

--- app/javascript/controllers/rails_bytes_controller.js
+++ app/javascript/controllers/rails_bytes_controller.js
@@ -4,23 +4,22 @@ export default class extends Controller {
   static targets = [ "name" ]
     
   update() {
-    
-    
     const railBytesCombos = JSON.parse(document.getElementById('rails-bytes-combos').textContent)
     
     const selectedRailsBytes = [...document.querySelectorAll("#tabs-classics [data-id=rails-menu-card] input")].filter(x => x.checked);
     const selectedRailsBytesIds = selectedRailsBytes.map(item => item.id )
-    
-    console.log(selectedRailsBytesIds)
-    
+        
     const railsBytesCombosKey = selectedRailsBytesIds.sort().map(id => id.replace('classics-tab-','')).join('@')
-    
-    console.log(railsBytesCombosKey)
+      
     
     const output = railBytesCombos[railsBytesCombosKey]
     const spacedOutput = ' ' + '--template ' + output
     const styledOutput = (output === '' ? '' : spacedOutput)
         
     document.getElementById('rails-bytes').textContent = styledOutput
+    
+    // Lock Railsbytes
+    
+    
   }
 }
