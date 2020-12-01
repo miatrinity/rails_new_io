ad75824264a274fbfe26463f92097662e29e07f3
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-12-01 09:51:42 +0100

    Updated Tailwind to 2.0

--- app/javascript/config/tailwind.config.js
+++ app/javascript/config/tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue'
+    // Add any other JS files here (i.e. .jsx, .ts, etc...)
+  ],  
   variants: {
     margin: ['responsive', 'first'],
   },
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-require('autoprefixer'),
-require('postcss-import'),
+    require('tailwindcss')('./app/javascript/config/tailwind.config.js'),
+    require('autoprefixer'),
+    require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
