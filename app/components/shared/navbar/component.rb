e9430e3d141897ef5ccefc9305a9c68a3da40b0f
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-12-04 11:17:35 +0100

    Refactor: rethink navbar HTML and CSS

--- app/components/shared/navbar/component.rb
+++ app/components/shared/navbar/component.rb
@@ -1,7 +1,20 @@
 module Shared
   module Navbar
     class Component < ViewComponent::Base
-      def initialize; end
+      def initialize
+        @items = { 
+          '🏠️ Home': '', 
+          '🤔 Why?': 'why', 
+          '📽️ Live Demo': 'live-demo',
+          '📨 About / Contact': 'about'
+          }
+      end
+
+      def sitcky_or_not(controller_name)
+        return '' unless controller_name == 'pages'
+
+        'sticky top-0'
+      end
     end
   end
 end
--- app/javascript/config/tailwind.config.js
+++ app/javascript/config/tailwind.config.js
@@ -11,5 +11,5 @@ module.exports = {
   ],    
   variants: {
     margin: ['responsive', 'first'],
-  },
+  }
 }
