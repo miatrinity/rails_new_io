5d0298ac3d70b1a096a3021b574c29d148cb5ab3
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2021-01-19 13:49:22 +0100

    Delete old RailsBytes lock setup

--- app/controllers/initial_setup_controller.rb
+++ app/controllers/initial_setup_controller.rb
@@ -9,56 +9,5 @@ class InitialSetupController < ApplicationController
     @all_menu_cards_in_all_states = Rails.configuration.all_menu_cards_in_all_states
     @rails_bytes_combos = Rails.configuration.rails_bytes_combos
     @rails_bytes_locks = Rails.configuration.rails_bytes_locks
-
-    @rails_bytes_locks = {
-      # frontend
-      'classics-tab-frontend-none': {},
-      'classics-tab-frontend-stimulus': {
-        'main-tab-starters-gemfile': true,
-        'main-tab-starters-bundle': true,
-        'main-tab-frontend-javascript': true,
-        'main-tab-frontend-webpacker': false,
-        'main-tab-frontend-turbolinks': true,
-        'main-tab-frontend-yarn': true
-      },
-      'classics-tab-frontend-stimulus-reflex': {
-        'main-tab-starters-gemfile': true,
-        'main-tab-starters-bundle': true,
-        'main-tab-mains-actioncable': true,
-        'main-tab-frontend-javascript': true,
-        'main-tab-frontend-webpacker': false,
-        'main-tab-frontend-turbolinks': true,
-        'main-tab-frontend-yarn': true
-      },
-      # testing
-      'classics-tab-testing-none': {
-        'main-tab-testing-minitest': false,
-        'main-tab-testing-system': false
-      },
-      'classics-tab-testing-minitest': {
-        'main-tab-testing-minitest': true
-      },
-      'classics-tab-testing-rspec': {
-        'main-tab-testing-minitest': false
-      },
-      # css
-      'classics-tab-css-none': {},
-      'classics-tab-css-tailwind': {
-        'main-tab-starters-gemfile': true,
-        'main-tab-starters-bundle': true,
-        'main-tab-frontend-javascript': true,
-        'main-tab-frontend-webpacker': false,
-        'main-tab-frontend-turbolinks': true,
-        'main-tab-frontend-yarn': true
-      },
-      'classics-tab-css-bootstrap': {
-        'main-tab-starters-gemfile': true,
-        'main-tab-starters-bundle': true,
-        'main-tab-frontend-javascript': true,
-        'main-tab-frontend-webpacker': false,
-        'main-tab-frontend-turbolinks': true,
-        'main-tab-frontend-yarn': true
-      }
-    }
   end
 end
