c5ec11ff0f987e812ff7bf538044baa42e8b0c5e
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2021-02-16 14:07:49 +0100

    Update test

--- test/system/admin/app_recipes/index_test.rb
+++ test/system/admin/app_recipes/index_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class AdminAppRecipeIndexTest < ApplicationSystemTestCase
   setup do
     @app_recipe = app_recipes(:stimulus_tailwind)
-    @last_verification_run = VerificationRun.last
+    @last_verification_run = @app_recipe.verification_runs.last
   end
 
   test "App recipes are correctly rendered in admin index view" do
@@ -11,6 +11,6 @@ class AdminAppRecipeIndexTest < ApplicationSystemTestCase
 
     assert_text @app_recipe.title
     assert_text @last_verification_run.status
-    assert_text "less than a minute ago"
+    assert_text "#{time_ago_in_words(@last_verification_run.finished_at)} ago"
   end
 end
