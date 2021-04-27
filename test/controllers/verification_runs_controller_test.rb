5863212e788556194fa783c97a4754daa694af4c
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2021-04-27 13:13:05 +0200

    Test Github pingback - verification run should end up in either finished_with_success or finished_with_failure state

--- test/controllers/verification_runs_controller_test.rb
+++ test/controllers/verification_runs_controller_test.rb
@@ -7,20 +7,19 @@ class VerificationRunsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update verification_run for success" do
     patch admin_verification_run_url(@verification_run.app_recipe.app_name),
-          params: { github_status: 'success' },
-          headers: { "Authorization": "Token #{@verification_run.app_recipe.user.api_token}" }
-          
+      params: {github_status: "success"},
+      headers: {"Authorization": "Token #{@verification_run.app_recipe.user.api_token}"}
+
     @verification_run.reload
     assert_equal "finished_with_success", @verification_run.state
   end
-  
+
   test "should update verification_run for failure" do
     patch admin_verification_run_url(@verification_run.app_recipe.app_name),
-          params: { github_status: 'whatever' },
-          headers: { "Authorization": "Token #{@verification_run.app_recipe.user.api_token}" }
-          
+      params: {github_status: "whatever"},
+      headers: {"Authorization": "Token #{@verification_run.app_recipe.user.api_token}"}
+
     @verification_run.reload
     assert_equal "finished_with_failure", @verification_run.state
   end
 end
-
